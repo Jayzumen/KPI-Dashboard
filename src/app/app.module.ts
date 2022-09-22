f2941fb535f51ea8753e56b4ed16b3d60f77deb5
jayzumen <reinhardtjanniklas96@gmail.com>
2022-09-22 17:05:19 +0200

    Revert "login attempt 1"
    This reverts commit d276e3506dda3542c0084c4295ea71efe71929f2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
+<<<<<<< HEAD
+=======
+import { LoginComponent } from './login/login.component';
+>>>>>>> parent of d276e35 (login attempt 1)
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     OverviewComponent
+=======
+    OverviewComponent,
+    LoginComponent
+>>>>>>> parent of d276e35 (login attempt 1)
   ],
   imports: [
     BrowserModule,
