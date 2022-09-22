b7de0ea1e30e2c4ddd8374ff777c6fadb3aa1961
jayzumen <reinhardtjanniklas96@gmail.com>
2022-09-22 17:07:01 +0200

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
-<<<<<<< HEAD
-=======
-import { LoginComponent } from './login/login.component';
->>>>>>> parent of d276e35 (login attempt 1)
 
 @NgModule({
-  declarations: [
-    AppComponent,
-<<<<<<< HEAD
-    OverviewComponent
-=======
-    OverviewComponent,
-    LoginComponent
->>>>>>> parent of d276e35 (login attempt 1)
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, OverviewComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
